885135da899c60227aa79b230e9ebbc49e0b65d8
HoneyB <qudgus9601@naver.com>
2023-03-19 03:14:49 +0900

    [FIX] 포멧 CRUD API 적용

--- client/src/pages/admin/Setting.jsx
+++ client/src/pages/admin/Setting.jsx
@@ -1,18 +1,14 @@
 import axios from "axios";
 import { useEffect, useState, useRef } from "react";
 
+// FIX : 200개 체크하기
+
 const Setting = () => {
   const [list, setList] = useState([]);
   const [input, setInput] = useState("");
   const inputRef = useRef();
   useEffect(() => {
-    axios({
-      method: "GET",
-      url: `${process.env.REACT_APP_SERVER_URL}/api/format/list`,
-      withCredentials: true,
-    }).then((data) => {
-      setList(data.data.formatList);
-    });
+    getList();
   }, []);
 
   useEffect(() => {});
@@ -45,30 +41,32 @@ const Setting = () => {
    * @desc 포멧을 추가합니다.
    */
   const addFormat = () => {
-    if (input.length > 0 && input.length <= 20) {
-      axios({
-        method: "POST",
-        url: `${process.env.REACT_APP_SERVER_URL}/api/format/add`,
-        data: { name: input },
-        withCredentials: true,
-      }).then((data) => {
-        if (!!data.data.formatInfo) {
-          axios({
-            method: "GET",
-            url: `${process.env.REACT_APP_SERVER_URL}/api/format/list`,
-            withCredentials: true,
-          }).then((data) => {
-            setList(data.data.formatList);
-            setInput("");
-            inputRef.current.value = "";
-            inputRef.current.classList.remove("border-2");
-            inputRef.current.classList.remove("border-red-700");
-          });
-        }
-      });
+    const len = list.filter((e) => {
+      return e.name === input;
+    }).length;
+
+    if (len !== 0) {
+      window.alert("이미 등록 되어 있는 포멧입니다.");
     } else {
-      inputRef.current.classList.add("border-2");
-      inputRef.current.classList.add("border-red-700");
+      if (input.length > 0 && input.length <= 20) {
+        axios({
+          method: "POST",
+          url: `${process.env.REACT_APP_SERVER_URL}/api/format/add`,
+          data: { name: input },
+          withCredentials: true,
+        }).then((data) => {
+          inputRef.current.value = "";
+          inputRef.current.classList.remove("border-2");
+          inputRef.current.classList.remove("border-red-700");
+          setInput("");
+          if (!!data.data.formatInfo) {
+            getList();
+          }
+        });
+      } else {
+        inputRef.current.classList.add("border-2");
+        inputRef.current.classList.add("border-red-700");
+      }
     }
   };
 
@@ -84,18 +82,43 @@ const Setting = () => {
     })
       .then((data) => {
         if (data.data.success) {
-          axios({
-            method: "GET",
-            url: `${process.env.REACT_APP_SERVER_URL}/api/format/list`,
-            withCredentials: true,
-          }).then((data) => {
-            setList(data.data.formatList);
-          });
+          getList();
         }
       })
       .catch((error) => {
         console.log(error);
       });
+  };
+
+  /**
+   * @desc 포멧을 제한합니다
+   */
+  const deprecateToggle = (e, deprecated) => {
+    console.log(deprecated);
+    axios({
+      method: "POST",
+      url: `${process.env.REACT_APP_SERVER_URL}/api/format/deprecate`,
+      data: { name: e.currentTarget.id, deprecated: deprecated },
+      withCredentials: true,
+    }).then((data) => {
+      if (data.data.success) {
+        getList();
+      }
+    });
+  };
+
+  /**
+   * @desc 모든 포멧 리스트를 받아옵니다.
+   */
+  const getList = () => {
+    axios({
+      method: "GET",
+      url: `${process.env.REACT_APP_SERVER_URL}/api/format/list`,
+      withCredentials: true,
+    }).then((data) => {
+      const formatList = data.data.formatList;
+      setList(formatList);
+    });
   };
 
   return (
@@ -114,12 +137,22 @@ const Setting = () => {
             ?.filter((e) => {
               return e.static === true;
             })
+            ?.sort((a, b) => {
+              if (a.name > b.name) return 1;
+              if (a.name < b.name) return -1;
+              return 0;
+            })
             .map((e, idx) => {
               return (
                 <div
-                  className="bg-gray-100 text-black cursor-pointer w-20 h-10 flex flex-row justify-center items-center rounded mr-4 hover:scale-105"
+                  className={`${
+                    e.deprecated ? "bg-blue-500" : "bg-gray-100"
+                  } text-black cursor-pointer w-20 h-10 flex flex-row justify-center items-center rounded mr-4 hover:scale-105`}
                   key={idx}
                   id={e.name}
+                  onClick={(event) => {
+                    deprecateToggle(event, e.deprecated);
+                  }}
                 >
                   <span className="mx-1">{e.name}</span>
                   <span className="inline-flex items-center p-1 mr-1 text-sm font-semibold text-gray-800 bg-transparent">
@@ -132,7 +165,11 @@ const Setting = () => {
                     >
                       <path
                         fillRule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        d={
+                          e.deprecated
+                            ? "M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                            : "M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0zz"
+                        }
                         clipRule="evenodd"
                       ></path>
                     </svg>
